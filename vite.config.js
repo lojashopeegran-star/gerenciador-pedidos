2d7b0ae8e4ed114f48850336c9f73b0146fdac56
lojashopeegran-star <lojashopeegran@gmail.com>
2026-06-29 00:21:15 -0300

    fix: xlsx via CDN resolve TDZ

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-})  
-// v2
+  build: {
+    rollupOptions: {
+      external: ['xlsx'],
+      output: {
+        globals: {
+          xlsx: 'XLSX'
+        }
+      }
+    }
+  },
+})
