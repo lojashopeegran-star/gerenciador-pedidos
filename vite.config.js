b17b4e3270935d60315d7d1b57d90932bbb95107
lojashopeegran-star <lojashopeegran@gmail.com>
2026-06-29 00:35:47 -0300

    fix: formato IIFE resolve TDZ

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: 'es2015',
     rollupOptions: {
       external: ['xlsx'],
       output: {
-        globals: {
-          xlsx: 'XLSX'
-        }
+        globals: { xlsx: 'XLSX' },
+        format: 'iife',
       }
     }
   },
